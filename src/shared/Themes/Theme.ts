af93494a56c068d9a3eb8483293bdbaf3690cadc
Alan Manoel <alanmanoel517@gmail.com>
2026-07-23 09:40:14 -0300

    feat: adding themes

---
+++ src/shared/Themes/Theme.ts
@@ -0,0 +1,25 @@
+
+
+
+export const Theme = {
+    colors: {
+        neutralBlack: "#171717",
+        neutralWhite: "#F3F0FA",
+
+        primary500: "#0087F7",
+
+    },
+    fonts: {
+        poppinsRegular: "poppinsRegular",
+        poppinsBold: "poppinsBold",
+    },
+    fontSize: {
+        body: 20,
+        button: 14,
+        caption: 12,
+
+        h3: 40,
+        h5: 24,
+        h6: 20,
+    }
+}
