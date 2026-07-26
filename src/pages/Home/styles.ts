e065540e83edd62dfaf41a08d48a368c9d11a48f
Alan Manoel <alanmanoel517@gmail.com>
2026-07-26 15:12:05 -0300

    feat: adjusting styles

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,6 +1,38 @@
+import { Theme } from "@/shared/Themes";
 import { StyleSheet } from "react-native";
 
 
 
 export const styles = StyleSheet.create({
-})
+    container: {
+        paddingHorizontal: 20,
+        paddingTop: 36,
+    },
+    header: {
+        flexDirection: "row",
+        justifyContent: "center",
+        paddingBottom: 48,
+    },
+    containerLogo: {
+        flex: 1,
+        flexDirection: "row",
+        gap: 12,
+    },
+    title: {
+        fontSize: Theme.fontSize.body,
+        fontFamily: Theme.fonts.poppinsRegular,
+        lineHeight: 18,
+    },
+    subtitle: {
+        fontSize: Theme.fontSize.h6,
+        fontFamily: Theme.fonts.poppinsBold
+    },
+    description: {
+        fontFamily: Theme.fonts.poppinsBold,
+        fontSize: Theme.fontSize.h5,
+        marginBottom: 20
+    },
+    categoriesContaine: {
+        gap: 8, paddingHorizontal: 20
+    },
+});
