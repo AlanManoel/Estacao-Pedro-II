16fedf56157623b61a0081e07743a642a454979d
Alan Manoel <alanmanoel517@gmail.com>
2026-07-27 09:41:10 -0300

    feat: modifield paths

--- src/shared/Assets/index.ts
+++ src/shared/Assets/index.ts
@@ -3,4 +3,7 @@
 
 export const Images = {
   logoBlue: require('../../../assets/iconLogoBlue.png'),
+  saltoLiso: require("./Images/saltoLiso.jpg"),
+  urubuRei: require("./Images/urubuRei.jpg"),
+  samambaia: require("./Images/samambaia.jpg"),
 };
