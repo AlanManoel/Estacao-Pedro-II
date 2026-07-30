03da476477eafe1e5338f20cf8630d5f9f5cfae8
Alan Manoel <alanmanoel517@gmail.com>
2026-07-30 10:33:24 -0300

    feat: create data waterfalls

---
+++ src/Data/waterfalls.ts
@@ -0,0 +1,35 @@
+import { Images } from "@/shared/Assets"
+
+
+export const waterfalls = [
+    {
+        id: 1,
+        name: "Cachoeira do Salto Liso",
+        image: Images.saltoLiso,
+        time: "30min",
+        distance: "2km",
+        level: "Média",
+        description:
+            "A Cachoeira do Salto Liso em Pedro II é um belo ponto de ecoturismo que possui uma queda d'água de cerca de 26 a 35 metros, uma piscina natural de águas claras e um trajeto desafiador com trilhas de terra e pedras escorregadias",
+    },
+    {
+        id: 2,
+        name: "Cachoeira do Urubu rei",
+        image: Images.urubuRei,
+        time: "45min",
+        distance: "2,3km",
+        level: "Difícil",
+        description:
+            "A Cachoeira do Urubu Rei em Pedro II é a maior queda d'água do estado do Piauí, destacando-se por seus imponentes 76 metros de altura, por ser perene (não secar nunca) e por exigir uma trilha de nível médio em meio à mata",
+    },
+    {
+        id: 3,
+        name: "Cachoeira da Samambaia",
+        image: Images.samambaia,
+        time: "20min",
+        distance: "2km",
+        level: "Média",
+        description:
+            "A Cachoeira da Samambaia é um atrativo natural em Pedro II que faz parte do Circuito das Cachoeiras, junto com a Cachoeira do Buriti e a Cachoeira do Lajeiro.",
+    },
+] 
