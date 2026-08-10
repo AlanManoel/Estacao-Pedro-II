04d8e197e32fdd5847ee99da4bb91b5ad1f6fb26
Alan Manoel <alanmanoel517@gmail.com>
2026-08-10 08:21:10 -0300

    add data tourism

--- src/Data/touristAttractions.ts
+++ src/Data/touristAttractions.ts
@@ -12,7 +12,34 @@ export const touristAttractions = [
             longitude: -41.4587,
         },
 
-        distance: "12 km",
+        duration: "10 minutos",
+
+        description:
+            "O Morro do Gritador, em Pedro II (PI), é um mirante famoso por suas vistas panorâmicas das serras e cânions. O local combina natureza, clima agradável e uma das paisagens mais bonitas da região.",
+
+        photos: [
+            //   Images.miranteGritador1,
+            //   Images.miranteGritador2,
+            //   Images.miranteGritador3,
+        ],
+
+        tips:
+            "No fim da tarde para aproveitar o pôr do sol. Aproveite para experimentar a culinária local. Leve câmera ou celular para boas fotos da paisagem.",
+
+        openingHours: "08:00 às 17:00",
+
+        price: "Entrada gratuita",
+    },
+    {
+        id: 2,
+        name: "Memorial Tertuliano Brandão Filho",
+
+        image: Images.memorial,
+
+        location: {
+            latitude: -4.4255,
+            longitude: -41.4587,
+        },
 
         duration: "10 minutos",
 
