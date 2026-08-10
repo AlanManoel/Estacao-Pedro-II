464b92cef79a2add9c89bef540bd7567a0140f05
Alan Manoel <alanmanoel517@gmail.com>
2026-08-10 08:17:30 -0300

    feat: add data de events

---
+++ src/Data/event.ts
@@ -0,0 +1,15 @@
+import { Images } from "@/shared/Assets";
+
+
+
+export const events = [
+    {
+        id: 1,
+        name: "Festival de inverno",
+        image: Images.festivalInverno,
+        date: "Ocorre entre os meses de junho e julho",
+        description:
+            "A Cachoeira do Salto Liso em Pedro II é um belo ponto de ecoturismo que possui uma queda d'água de cerca de 26 a 35 metros, uma piscina natural de águas claras e um trajeto desafiador com trilhas de terra e pedras escorregadias",
+    },
+
+]
